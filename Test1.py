0c0a63a59e05f21066c592995e2ff039b5e014ed
Islek1 <zakiisl@yandex.ru>
2023-12-27 14:22:44 +0300

    Snow

--- Test1.py
+++ Test1.py
@@ -14,5 +14,5 @@ numeric_columns = data.select_dtypes(include=['number'])
 numeric_columns = data.select_dtypes(include=['int64', 'float64'])
 X = numeric_columns.drop(columns=['G3'])
 y = data['G3']
-
+g
 
